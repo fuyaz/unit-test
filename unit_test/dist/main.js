c177017c2312e782f7716843b800015039cb14dd
Filldexnoi <96350693+Filldexnoi@users.noreply.github.com>
2024-02-12 00:25:18 +0700

    update

--- unit_test/dist/main.js
+++ unit_test/dist/main.js
@@ -18,6 +18,42 @@ var MergeFunction = {
                 k--;
             }
         }
+        if (i === collection1.length) {
+            while (j < collection2.length && k >= 0) {
+                if (collection2[j] <= collection3[k]) {
+                    result.push(collection2[j]);
+                    j++;
+                }
+                else {
+                    result.push(collection3[k]);
+                    k--;
+                }
+            }
+        }
+        else if (j === collection2.length) {
+            while (i < collection1.length && k >= 0) {
+                if (collection1[i] <= collection3[k]) {
+                    result.push(collection1[i]);
+                    i++;
+                }
+                else {
+                    result.push(collection3[k]);
+                    k--;
+                }
+            }
+        }
+        else if (k === -1) {
+            while (i < collection1.length && j < collection2.length) {
+                if (collection1[i] <= collection2[j]) {
+                    result.push(collection1[i]);
+                    i++;
+                }
+                else {
+                    result.push(collection2[j]);
+                    j++;
+                }
+            }
+        }
         while (i < collection1.length) {
             result.push(collection1[i]);
             i++;
